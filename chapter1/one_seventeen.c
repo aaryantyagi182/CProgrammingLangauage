7bfc7f769423869290663c0dbe1698a49910f5de
Ankur Singh <ramanujanankur@gmail.com>
2018-02-28 22:39:00 +0800

    1-17

--- chapter1/one_seventeen.c
+++ chapter1/one_seventeen.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#define MAXLINE 10
+#define MAXLINE 1000
+#define MIN_LENGTH 80
 
 int getline(char line[], int maxline);
 void copy(char form[], char to[]);
@@ -7,20 +8,15 @@ void copy(char form[], char to[]);
 
 int main(){
 	int len;
-	int max;
 	char line[MAXLINE];
 	char longest[MAXLINE];
 	
-	max = 0;
+
 	while((len = getline(line, MAXLINE)) > 0){
-		if(len >= max){
-			max = len;
-			copy(longest, line);
+		if(len >= MIN_LENGTH){
+			printf("%s",line);
 		}
-		if(max > 0){
-			printf("%d\n",len);
-			printf("%s",longest);
-		}
+			
 	
 	}
 	return 0;	
@@ -33,21 +29,10 @@ int getline(char s[], int lim){
 		s[i] = c;
 	
 		if(c == '\n' ){
-			if(i < lim - 1){
-				s[i] = c;
-			}
-			else{
-				s[i] = '\0';
-			}
+			s[i] = c;
 			i++;
 		}
-		else if(c != EOF){
-			i++;
-			while((c = getchar()) != EOF && c != '\n')
-				i++;
-		}
-		if(i <= lim  - 1)
-			s[i] = '\0';
+		s[i] = '\0';
 		return i;
 	
 }			
