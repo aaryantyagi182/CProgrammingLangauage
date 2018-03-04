ad1308fccf72cde0b4b0af5224acc1614a252ed0
Ankur Singh <ramanujanankur@gmail.com>
2018-03-04 16:51:38 +0800

    refactored one_twenty code. No use of butter input

--- chapter1/one_twenty.c
+++ chapter1/one_twenty.c
@@ -2,65 +2,34 @@
 
 #define MAXLENGTH 1000
 #define MAXLINE 80
-#define TAB_WIDTH 4
+#define TAB_WIDTH 10
 
-void detab(char line[], int lim);
-int getline(char s[], int lim);
-void replace_tab(char line[], int last_char, int pos, int lim);
-int arr_len(char line[], int lim);
-void shift_line(char line[], int end, int tab, int lim);
+void detab();
 
 int main() {
   char line[MAXLINE];
   int len = 0;
-  while ((len = getline(line, MAXLINE)) > 0) {
-    detab(line, MAXLINE);
-    printf("%s", line);
-  }
+  detab();
 }
 
-int getline(char s[], int lim) {
-  int c, i;
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-    s[i] = c;
-  if (c == '\n') {
-    s[i] = c;
-    i++;
-  }
-  s[i] = '\0';
-  return i;
-}
+void detab() {
+  int ch;
+  int nc = 0;
+  int ns = 0;
+  while ((ch = getchar()) != EOF) {
+    if (ch == '\t') {
+      ns = TAB_WIDTH - (nc % TAB_WIDTH);
+      nc = 0;
+      while (ns-- > 0) {
+        putchar(' ');
+      }
+    } else if (ch == '\n') {
+      putchar(ch);
+      nc = 0;
 
-void detab(char line[], int lim) {
-  int i = 0;
-  int last_char = arr_len(line, lim);
-  for (i = 0; i < lim && (line[i] != '\n' || line[i] != '\0'); i++) {
-    if (line[i] == '\t')
-      replace_tab(line, i, last_char, lim);
-  }
-}
-
-void replace_tab(char line[], int pos, int last_char, int lim) {
-  shift_line(line, last_char, pos, lim);
-}
-
-int arr_len(char line[], int lim) {
-  int last_char = 0;
-  for (; last_char < lim && line[last_char] != '\0'; last_char++)
-    ;
-  return last_char;
-}
-
-void shift_line(char line[], int end, int tab, int lim) {
-  if (end + TAB_WIDTH - 1 >= lim)
-    return;
-  for (; end > tab; end--) {
-    line[end + TAB_WIDTH - 1] = line[end];
-  }
-  // Replace tab with black spaces
-  // Move end to the place where to start filling with blank space
-  end = end + TAB_WIDTH - 1;
-  while (end >= tab) {
-    line[end--] = ' ';
+    } else {
+      putchar(ch);
+      nc++;
+    }
   }
 }
